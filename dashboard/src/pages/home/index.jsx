14d7f19072018f685e2a97ca2fd946039b699efa
pedrofzero <48291490+pedrofzero@users.noreply.github.com>
2024-06-23 23:37:55 +0100

    Add more placeholder home info

---
+++ dashboard/src/pages/home/index.jsx
@@ -0,0 +1,301 @@
+import Title from "@/components/ui/Title";
+import { Avatar, AvatarFallback, AvatarImage } from "@/shadcn/ui/avatar";
+import { Button } from "@/shadcn/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/shadcn/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/shadcn/ui/tabs";
+
+import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts"
+
+const Home = () => {
+
+    const data = [
+        {
+            name: "Jan",
+            total: Math.floor(Math.random() * 5000) + 1000,
+        },
+        {
+            name: "Feb",
+            total: Math.floor(Math.random() * 5000) + 1000,
+        },
+        {
+            name: "Mar",
+            total: Math.floor(Math.random() * 5000) + 1000,
+        },
+        {
+            name: "Apr",
+            total: Math.floor(Math.random() * 5000) + 1000,
+        },
+        {
+            name: "May",
+            total: Math.floor(Math.random() * 5000) + 1000,
+        },
+        {
+            name: "Jun",
+            total: Math.floor(Math.random() * 5000) + 1000,
+        },
+        {
+            name: "Jul",
+            total: Math.floor(Math.random() * 5000) + 1000,
+        },
+        {
+            name: "Aug",
+            total: Math.floor(Math.random() * 5000) + 1000,
+        },
+        {
+            name: "Sep",
+            total: Math.floor(Math.random() * 5000) + 1000,
+        },
+        {
+            name: "Oct",
+            total: Math.floor(Math.random() * 5000) + 1000,
+        },
+        {
+            name: "Nov",
+            total: Math.floor(Math.random() * 5000) + 1000,
+        },
+        {
+            name: "Dec",
+            total: Math.floor(Math.random() * 5000) + 1000,
+        },
+    ]
+
+    return (
+        <div className="flex-1 space-y-4 p-8 pt-6">
+            <div className="flex items-center justify-between space-y-2">
+                <Title title="Dashboard" />
+                {/* <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2> */}
+                <div className="flex items-center space-x-2">
+                    {/* <CalendarDateRangePicker /> */}
+                    <Button>Download</Button>
+                </div>
+            </div>
+            <Tabs defaultValue="overview" className="space-y-4">
+                <TabsList>
+                    <TabsTrigger value="overview">Overview</TabsTrigger>
+                    <TabsTrigger value="analytics" disabled>
+                        Analytics
+                    </TabsTrigger>
+                    <TabsTrigger value="reports" disabled>
+                        Reports
+                    </TabsTrigger>
+                    <TabsTrigger value="notifications" disabled>
+                        Notifications
+                    </TabsTrigger>
+                </TabsList>
+                <TabsContent value="overview" className="space-y-4">
+                    <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+                        <Card>
+                            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                <CardTitle className="text-sm font-medium">
+                                    Total Revenue
+                                </CardTitle>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    className="h-4 w-4 text-muted-foreground"
+                                >
+                                    <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
+                                </svg>
+                            </CardHeader>
+                            <CardContent>
+                                <div className="text-2xl font-bold">$45,231.89</div>
+                                <p className="text-xs text-muted-foreground">
+                                    +20.1% from last month
+                                </p>
+                            </CardContent>
+                        </Card>
+                        <Card>
+                            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                <CardTitle className="text-sm font-medium">
+                                    Subscriptions
+                                </CardTitle>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    className="h-4 w-4 text-muted-foreground"
+                                >
+                                    <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+                                    <circle cx="9" cy="7" r="4" />
+                                    <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
+                                </svg>
+                            </CardHeader>
+                            <CardContent>
+                                <div className="text-2xl font-bold">+2350</div>
+                                <p className="text-xs text-muted-foreground">
+                                    +180.1% from last month
+                                </p>
+                            </CardContent>
+                        </Card>
+                        <Card>
+                            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                <CardTitle className="text-sm font-medium">Sales</CardTitle>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    className="h-4 w-4 text-muted-foreground"
+                                >
+                                    <rect width="20" height="14" x="2" y="5" rx="2" />
+                                    <path d="M2 10h20" />
+                                </svg>
+                            </CardHeader>
+                            <CardContent>
+                                <div className="text-2xl font-bold">+12,234</div>
+                                <p className="text-xs text-muted-foreground">
+                                    +19% from last month
+                                </p>
+                            </CardContent>
+                        </Card>
+                        <Card>
+                            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                <CardTitle className="text-sm font-medium">
+                                    Active Now
+                                </CardTitle>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    className="h-4 w-4 text-muted-foreground"
+                                >
+                                    <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
+                                </svg>
+                            </CardHeader>
+                            <CardContent>
+                                <div className="text-2xl font-bold">+573</div>
+                                <p className="text-xs text-muted-foreground">
+                                    +201 since last hour
+                                </p>
+                            </CardContent>
+                        </Card>
+                    </div>
+                    <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
+                        <Card className="col-span-4">
+                            <CardHeader>
+                                <CardTitle>Overview</CardTitle>
+                            </CardHeader>
+                            <CardContent className="pl-2">
+                                {/* <Overview /> */}
+                                <ResponsiveContainer width="100%" height={350}>
+                                    <BarChart data={data}>
+                                        <XAxis
+                                            dataKey="name"
+                                            stroke="#888888"
+                                            fontSize={12}
+                                            tickLine={false}
+                                            axisLine={false}
+                                        />
+                                        <YAxis
+                                            stroke="#888888"
+                                            fontSize={12}
+                                            tickLine={false}
+                                            axisLine={false}
+                                            tickFormatter={(value) => `$${value}`}
+                                        />
+                                        <Bar
+                                            dataKey="total"
+                                            fill="currentColor"
+                                            radius={[4, 4, 0, 0]}
+                                            className="fill-primary"
+                                        />
+                                    </BarChart>
+                                </ResponsiveContainer>
+                            </CardContent>
+                        </Card>
+                        <Card className="col-span-3">
+                            <CardHeader>
+                                <CardTitle>Recent Sales</CardTitle>
+                                <CardDescription>
+                                    You made 265 sales this month.
+                                </CardDescription>
+                            </CardHeader>
+                            <CardContent>
+                                <div className="space-y-8">
+                                    <div className="flex items-center">
+                                        <Avatar className="h-9 w-9">
+                                            <AvatarImage src="/avatars/01.png" alt="Avatar" />
+                                            <AvatarFallback>OM</AvatarFallback>
+                                        </Avatar>
+                                        <div className="ml-4 space-y-1">
+                                            <p className="text-sm font-medium leading-none">Olivia Martin</p>
+                                            <p className="text-sm text-muted-foreground">
+                                                olivia.martin@email.com
+                                            </p>
+                                        </div>
+                                        <div className="ml-auto font-medium">+$1,999.00</div>
+                                    </div>
+                                    <div className="flex items-center">
+                                        <Avatar className="flex h-9 w-9 items-center justify-center space-y-0 border">
+                                            <AvatarImage src="/avatars/02.png" alt="Avatar" />
+                                            <AvatarFallback>JL</AvatarFallback>
+                                        </Avatar>
+                                        <div className="ml-4 space-y-1">
+                                            <p className="text-sm font-medium leading-none">Jackson Lee</p>
+                                            <p className="text-sm text-muted-foreground">jackson.lee@email.com</p>
+                                        </div>
+                                        <div className="ml-auto font-medium">+$39.00</div>
+                                    </div>
+                                    <div className="flex items-center">
+                                        <Avatar className="h-9 w-9">
+                                            <AvatarImage src="/avatars/03.png" alt="Avatar" />
+                                            <AvatarFallback>IN</AvatarFallback>
+                                        </Avatar>
+                                        <div className="ml-4 space-y-1">
+                                            <p className="text-sm font-medium leading-none">Isabella Nguyen</p>
+                                            <p className="text-sm text-muted-foreground">
+                                                isabella.nguyen@email.com
+                                            </p>
+                                        </div>
+                                        <div className="ml-auto font-medium">+$299.00</div>
+                                    </div>
+                                    <div className="flex items-center">
+                                        <Avatar className="h-9 w-9">
+                                            <AvatarImage src="/avatars/04.png" alt="Avatar" />
+                                            <AvatarFallback>WK</AvatarFallback>
+                                        </Avatar>
+                                        <div className="ml-4 space-y-1">
+                                            <p className="text-sm font-medium leading-none">William Kim</p>
+                                            <p className="text-sm text-muted-foreground">will@email.com</p>
+                                        </div>
+                                        <div className="ml-auto font-medium">+$99.00</div>
+                                    </div>
+                                    <div className="flex items-center">
+                                        <Avatar className="h-9 w-9">
+                                            <AvatarImage src="/avatars/05.png" alt="Avatar" />
+                                            <AvatarFallback>SD</AvatarFallback>
+                                        </Avatar>
+                                        <div className="ml-4 space-y-1">
+                                            <p className="text-sm font-medium leading-none">Sofia Davis</p>
+                                            <p className="text-sm text-muted-foreground">sofia.davis@email.com</p>
+                                        </div>
+                                        <div className="ml-auto font-medium">+$39.00</div>
+                                    </div>
+                                </div>
+
+                            </CardContent>
+                        </Card>
+                    </div>
+                </TabsContent>
+            </Tabs>
+        </div>
+    )
+}
+
+export default Home;
