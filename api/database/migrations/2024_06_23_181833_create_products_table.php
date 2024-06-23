69b77cb863325f06ec43d74bd8ddf68c6fe274e5
pedrofzero <48291490+pedrofzero@users.noreply.github.com>
2024-06-23 19:55:52 +0100

    Create Product Model

---
+++ api/database/migrations/2024_06_23_181833_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->uuid('id')->primary()->default(DB::raw('(UUID())'));
+            $table->timestamps();
+
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->string('logo')->nullable();
+            $table->foreignId('user_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
