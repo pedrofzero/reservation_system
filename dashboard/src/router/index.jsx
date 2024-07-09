2e832907fd93bb9d152dfc13663e1833bba3cd75
pedrofzero <48291490+pedrofzero@users.noreply.github.com>
2024-07-09 22:12:48 +0100

    Add the new routes to router

--- dashboard/src/router/index.jsx
+++ dashboard/src/router/index.jsx
@@ -2,13 +2,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { AxiosInterceptor } from '@/helpers/api'
 
-import Login from '@/pages/login'
-import Register from '@/pages/register'
 import AuthLayout from '@/components/layout/AuthLayout'
 import DashboardLayout from '@/components/layout/DashboardLayout'
 
+import Login from '@/pages/login'
+import Register from '@/pages/register'
+
 import Home from '@/pages/home'
 import Products from '@/pages/products'
+import ProductDetails from '@/pages/products/details'
+
 import Profile from '@/pages/profile'
 import Settings from '@/pages/settings'
 
@@ -23,7 +26,11 @@ const Router = () => {
                     <Route path="/" element={<AuthLayout />}>
                         <Route path="/" element={<DashboardLayout />}>
                             <Route index element={<Home />} />
+                            
                             <Route path="/products" element={<Products />} />
+                            <Route path="/product/:id/" element={<ProductDetails />} />
+                            <Route path="/product/:id/:tab" element={<ProductDetails />} />
+
                             <Route path="/profile" element={<Profile />} />
                             <Route path="/settings" element={<Settings />} />
                         </Route>
